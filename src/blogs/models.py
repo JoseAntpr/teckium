945d3aab02847cb649a9ca14a26374cb7408d762
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-09-06 00:39:01 +0200

    First version of users and blogs django models

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+DRAFT = 1
+PUBLISHED = 2
+
+PUBLICATION_STATUS = ((DRAFT, "Draft"),(PUBLISHED, "Published"))
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=250)
+    logo = models.ImageField()
+    creation_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    image = models.ImageField()
+    summary = models.CharField(max_length=300)
+    content = models.TextField()
+    publication_date = models.DateField()
+    modified_date = models.DateField(auto_now=True)
+    status = models.IntegerField(choices=PUBLICATION_STATUS)
+
+    def __str__(self):
+        return title
+
+class Commentary(models.Model):
+    content = models.TextField()
+    def __str__(self):
+        return text
+ 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    def __str__(self):
+        return name
--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+RELATIONSHIP_FOLLOWING = 1
+RELATIONSHIP_BLOCKED = 2
+RELATIONSHIP_STATUSES = ((RELATIONSHIP_FOLLOWING, 'Following'), (RELATIONSHIP_BLOCKED, 'Blocked'))
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key = True, on_delete=models.CASCADE)
+    avatar = models.ImageField()
+    bio  = models.CharField(max_length=250, blank=True, null=True)
+    creation_date = models.DateField(auto_now_add=True)
+    relationships = models.ManyToManyField("self", through= 'Relationship', symmetrical=False, related_name='related_to', blank=True)
+
+    def __str__(self):
+        return user.name
+  
+class Relationship(models.Model):
+    from_person = models.ForeignKey(Profile, related_name='from_people')
+    to_person = models.ForeignKey(Profile, related_name='to_people')
+    status = models.IntegerField(choices=RELATIONSHIP_STATUSES)
+
+    def __str__(self):
+        return str(self.from_person.user.username) + "sigue a " + str(self.to_person.user.username)
+
+    class Meta: 
+        unique_together = (('from_person', 'to_person'),)
+
+    
