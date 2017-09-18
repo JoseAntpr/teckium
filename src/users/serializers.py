68c76fa39e0185a3e5d7982bad13464b36e2bb02
David Álvarez <david11988@gmail.com>
2017-09-18 21:27:02 +0200

    Se crea API de usuarios para obtener detalle de un usuario y borrado

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -4,6 +4,13 @@ from rest_framework import serializers
 from users.models import Profile
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+
+
 class UserListSerializer(serializers.Serializer):
 
     id = serializers.ReadOnlyField()
@@ -17,18 +24,31 @@ class UserSerializer(UserListSerializer):
     last_name = serializers.CharField()
     password = serializers.CharField()
 
+    profile = ProfileSerializer()
+
     def create(self, validated_data):
-        return self.update(User(), validated_data)
+        profile_data = validated_data.pop('profile')
+        user = User.objects.create(**validated_data)
+        Profile.objects.create(user=user, **profile_data)
+
+        return user
 
     def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        profile = instance.profile
+
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         if validated_data.get('password'):
             instance.set_password(validated_data.get('password'))
+
         instance.save()
 
-        Profile.objects.create(user=instance)
+        profile.user = profile_data.get('is_premium_member', instance)
+
+        profile.save()
 
         return instance
+
