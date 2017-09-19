f944e9903d348902751bfbc7084038a7688ffdf3
David Álvarez <david11988@gmail.com>
2017-09-19 20:47:37 +0200

    Se crea API de usuarios para actualizar un usuario y su profile

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -46,9 +46,45 @@ class UserSerializer(UserListSerializer):
 
         instance.save()
 
-        profile.user = profile_data.get('is_premium_member', instance)
+        profile.user = instance
+        profile.avatar = profile_data.get('avatar', profile.avatar)
+        profile.bio = profile_data.get('bio', profile.bio)
 
         profile.save()
 
         return instance
 
+'''class UserSerializer(UserListSerializer):
+
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    password = serializers.CharField()
+
+    profile = ProfileSerializer()
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        user = User.objects.create(**validated_data)
+        Profile.objects.create(user=user, **profile_data)
+
+        return user
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+        profile = instance.profile
+
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        if validated_data.get('password'):
+            instance.set_password(validated_data.get('password'))
+
+        instance.save()
+
+        profile.user = instance
+
+        profile.save()
+
+        return instance
+'''
