b23efd0d74bc3768a624cdd1a38ac5eae39bb671
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-09-27 12:18:58 +0200

    serve media files urls added

--- src/teckium/urls.py
+++ src/teckium/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 import blogs
 
@@ -30,4 +32,4 @@ urlpatterns = [
     # DOC del API
     url(r'^$', include('rest_framework_docs.urls')),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
