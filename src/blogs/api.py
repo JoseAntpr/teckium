1adcce6de728dedc19845bb66d9abe26a2efd264
David Álvarez <david11988@gmail.com>
2017-10-17 20:36:22 +0200

    Se añade para poder filtrar por blog

--- src/blogs/api.py
+++ src/blogs/api.py
@@ -29,7 +29,7 @@ class PostListAPIView(generics.ListCreateAPIView):
     #authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     serializer_class = PostSerializer
     filter_backends = (DjangoFilterBackend, OrderingFilter)
-    filter_fields = ('id', 'title', 'status', 'tags', 'owner')
+    filter_fields = ('id', 'title', 'status', 'tags', 'owner', 'blog')
     ordering_fields = ('-publication_date',)
     ordering = ('-publication_date',)
 
